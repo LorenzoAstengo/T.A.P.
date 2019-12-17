f6b5c1c702e41c004659fba3d7dac7a4967ce76d
Lorenzo Astengo <S4368146@studenti.unige.it>
2019-12-17 10:07:36 +0100

    Refactored code, implemented DbContext and Address class

---
+++ Lab5/Entity Framework/BugTrackingUI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BugTrackingLogic;
+namespace BugTrackingUI {
+    class Program {
+        static void Main(string[] args) {
+            const string adminPw = "a123";
+            const string connectionString = @"Data Source=ARYA;Initial Catalog=Puffo;Integrated Security=SSPI; MultipleActiveResultSets=True";
+
+            
+            Console.ReadLine();
+        }
+    }
+}
